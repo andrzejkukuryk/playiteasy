5a3b21d6ddb09e749c45ebbc911769ff7ac6ad3a
andrzejkukuryk <101364440+andrzejkukuryk@users.noreply.github.com>
2023-08-22 14:16:57 +0200

    resize images

--- src/scenes/home/footer/footer.tsx
+++ src/scenes/home/footer/footer.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
-import logoGuitar from "assets/logoGuitarHead.png";
+import logoGuitar from "assets/logoGuitarHead.webp";
 import "./footer.css";
 import { SocialMedia } from "./socialMedia/socialMedia";
 
